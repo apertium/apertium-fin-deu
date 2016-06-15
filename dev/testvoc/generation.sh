0f9a436e53af1afc89359daf81ef41d8c0d145fc
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2016-06-15 12:35:24 +0200

    parallel stuff from unhammer

--- dev/testvoc/generation.sh
+++ dev/testvoc/generation.sh
@@ -3,7 +3,7 @@
 set -e -u
 
 # How many times to follow cycle when expanding with --hfst; gets slow if too high:
-declare -i CYCLES=3
+declare -i CYCLES=0
 
 if [[ $# -ge 1 && $1 = --hfst ]]; then
     HFST=true
@@ -77,13 +77,16 @@ analysis_expansion_hfst () {
     # give the "disambiguated" output, no forms
 }
 
+PYTHONPATH="$(dirname "$0"):${PYTHONPATH:-}"
+export PYTHONPATH
+
 split_ambig () {
     if command -V pypy3 &>/dev/null; then
         python=pypy3
     else
         python=python3
     fi
-    PYTHONPATH="$(dirname "$0"):${PYTHONPATH}" "${python}" -c '
+    "${python}" -c '
 from streamparser import parse_file, readingToString
 import sys
 for blank, lu in parse_file(sys.stdin, withText=True):
@@ -126,12 +129,15 @@ case ${lang1} in
     nno|nob) clb="<clb>" ;;
 esac
 
+export -f mode_after_tagger
+export -f split_ambig
+
 if $HFST; then
     if [[ ${dix} = guess ]]; then
         dix="${mode}".automorf.hfst
     fi
     analysis_expansion_hfst "${dix}" "${clb}" \
-        | mode_after_tagger modes/"${mode}".mode \
+        | parallel -j3 --pipe --block 10M -- bash -c \'mode_after_tagger modes/"${mode}".mode\' \
         | only_errs
 else
     if [[ ${dix} = guess ]]; then
