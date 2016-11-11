0082d5b151ec962e41754edb893206c6b23edace
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2016-11-11 16:49:15 +0100

    daily routine as a script

---
+++ dev/daily-routines.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+DAILYMAIL=texts/dw.de-Langsam-gesprochene-Nachrichten-$(date --iso).text
+if ! test -f ${DAILYMAIL} ; then
+    echo ${DAILYMAIL} ist nicht da, bitte herunterladen
+    exit 1
+fi
+make
+if ! apertium -d . deu-fin-debug < ${DAILYMAIL} |\
+    fgrep --colour=always '@' ; then
+    apertium -d . deu-fin < ${DAILYMAIL}
+fi
