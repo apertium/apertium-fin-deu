14ae1ec8ad99fba0d8a617709abc605366f741cd
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2017-08-07 16:06:23 +0200

    more corner chase handlung

--- dev/gender-classifier.bash
+++ dev/gender-classifier.bash
@@ -6,10 +6,16 @@ MONODIX=$HOME/sf.net/apertium/languages/apertium-deu/apertium-deu.deu.dix
 fgrep anyg < $BIDIX |\
     sed -e 's/^.*<r>//' -e 's/<s .*$//' > anyg-deu.wordlist
 
+echo > apertium-fin-deu.genders.sed.unsort
 while read w ; do
-    fgrep "lm=\"$w\"" $MONODIX | fgrep -v lower | egrep -v '__(np|adv|vb|pr)' |\
+    fgrep "lm=\"$w\"" $MONODIX | fgrep -v lower |\
+        egrep -v '__(np|adv|vb|pr|adj|pprep)' |\
         sed -e 's/^.*par n=[^_]*__n_//' -e 's/"\/.*$//' |\
-        sed -e "s@^.*\$@s:<r>$w\\(.*\\)</r></p><par n=\"anyg\"/>:<r>$w\\\\1<s n=\"\\0\"</r></p>:@" \
+        sed -e "s@^.*\$@s:<r>$w<\\\\(.*\\\\)</r></p><par n=\"anyg\"/>:<r>$w<\\\\1<s n=\"\\0\"/></r></p>:@" \
         >> apertium-fin-deu.genders.sed.unsort
+    if test $(fgrep "lm=\"$w\"" $MONODIX | fgrep -v lower |\
+            egrep -v '__(np|adv|vb|pr|adj|pprep)' | wc -l) -gt 1 ; then
+        echo Achtung: $w has many genders
+    fi
 done < anyg-deu.wordlist
-sort apertium-fin-deu.genders.sed.unsort > apertium-fin-deu.genders.sed
+sort apertium-fin-deu.genders.sed.unsort | uniq > apertium-fin-deu.genders.sed
