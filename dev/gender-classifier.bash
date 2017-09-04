f99b33c5bc9720d8d3c90f373e5765d6bf1b84f9
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2017-09-04 14:38:06 +0200

    classified lower case compound parts

--- dev/gender-classifier.bash
+++ dev/gender-classifier.bash
@@ -3,7 +3,7 @@
 BIDIX=apertium-fin-deu.fin-deu.dix
 MONODIX=$HOME/sf.net/apertium/languages/apertium-deu/apertium-deu.deu.dix
 
-fgrep anyg < $BIDIX |\
+fgrep anyg < $BIDIX | egrep '<r>[[:upper:]]' |\
     sed -e 's/^.*<r>//' -e 's/<s .*$//' > anyg-deu.wordlist
 
 echo > apertium-fin-deu.genders.sed.unsort
@@ -19,3 +19,22 @@ while read w ; do
     fi
 done < anyg-deu.wordlist
 sort apertium-fin-deu.genders.sed.unsort | uniq > apertium-fin-deu.genders.sed
+
+# lower hacks too
+fgrep anyg < $BIDIX | egrep '<r>[[:lower:]]' |\
+    sed -e 's/^.*<r>//' -e 's/<s .*$//' > anyg-deu-lower.wordlist
+
+echo > apertium-fin-deu.genders-lower.sed.unsort
+while read w ; do
+    fgrep -i "lm=\"$w\"" $MONODIX | fgrep -v lower |\
+        egrep -v '__(np|adv|vb|pr|adj|pprep)' |\
+        sed -e 's/^.*par n=[^_]*__n_//' -e 's/"\/.*$//' |\
+        sed -e "s@^.*\$@s:<r>$w<\\\\(.*\\\\)</r></p><par n=\"anyg\"/>:<r>$w<\\\\1<s n=\"\\0\"/></r></p>:@" \
+        >> apertium-fin-deu.genders-lower.sed.unsort
+    if test $(fgrep "lm=\"$w\"" $MONODIX | fgrep -v lower |\
+            egrep -v '__(np|adv|vb|pr|adj|pprep)' | wc -l) -gt 1 ; then
+        echo Achtung: $w has many genders
+    fi
+done < anyg-deu-lower.wordlist
+sort apertium-fin-deu.genders-lower.sed.unsort | uniq > apertium-fin-deu.genders-lower.sed
+
